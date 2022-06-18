b93062dfbcde83de992e3f2514fbce574e8251f9
Tsering Tsering <dtsering2@gmail.com>
2022-06-18 17:09:21 -0400

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainIDmain = document.getElementById("main");
+
+mainIDmain.remove();
+
+const newHeader = document.querySelector("h1#victory");
+
+newHeader.textContent ="YOUR-NAME is the champion";
+
